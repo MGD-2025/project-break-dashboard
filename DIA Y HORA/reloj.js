fd373586f432192e4375be58bc3631b968e218bf
María del Mar García <116bmargar@gmail.com>
2025-12-03 18:41:41 +0100

    cambios

--- DIA Y HORA/reloj.js
+++ DIA Y HORA/reloj.js
@@ -1,12 +1,35 @@
 
 const reloj = document.getElementById('reloj')
+const frase = document.getElementById('frase')
+let mensaje = ''
+
 function actualizarHora (){
     const horactual = new Date ()
     let horas = horactual.getHours().toString().padStart(2,0)
     let minutos = horactual.getMinutes().toString().padStart(2,0)
     let segundos = horactual.getSeconds().toString().padStart(2,0)
 
+if (horas>00 && horas<7){
+    mensaje=('Es hora de dormir')
+}
+else if (horas>=7 && horas<12){
+    mensaje=('A desayunar y coger energía')
+}
+else if (horas>=12 && horas<15){
+    mensaje=('Vamos a trabajar duro')
+}
+else if (horas>=15 && horas<18){
+    mensaje=('A comer y descansar')
+}
+else if (horas>=18 && horas<21){
+    mensaje=('Venga, trabajamos un ratito más')
+}
+else if (horas>=21 && horas<00){
+    mensaje=('Relájate un ratito')
+}
+
 reloj.innerHTML= `${horas}:${minutos}:${segundos}`
+frase.innerHTML=mensaje
 }
 
 actualizarHora()
