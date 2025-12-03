0963eb78973db4ee5e35ee4de4fd06b9d315af2a
María del Mar García <116bmargar@gmail.com>
2025-12-03 13:22:27 +0100

    dia y hora done

---
+++ DIA Y HORA/reloj.js
@@ -0,0 +1,30 @@
+
+const reloj = document.getElementById('reloj')
+function actualizarHora (){
+    const horactual = new Date ()
+    let horas = horactual.getHours().toString().padStart(2,0)
+    let minutos = horactual.getMinutes().toString().padStart(2,0)
+    let segundos = horactual.getSeconds().toString().padStart(2,0)
+
+reloj.innerHTML= `${horas}:${minutos}:${segundos}`
+}
+
+actualizarHora()
+setInterval(actualizarHora,1000)
+
+
+const fecha = document.getElementById ('fecha')
+const mesesaño = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre']
+const diassemana = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
+
+function actualizaFecha (){
+    const fechactual = new Date ()
+    let dia =fechactual.getDate()
+    let mes =mesesaño[fechactual.getMonth()]
+    let año =fechactual.getFullYear()
+    let diasemana = diassemana[fechactual.getDay ()]
+    
+    fecha.innerHTML = `Hoy es ${diasemana},  día ${dia} de ${mes} del ${año}`
+}
+
+actualizaFecha()
